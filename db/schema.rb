bda4ca851a9babbd10b799ed82e985f8762eac38
Potipecan <podpecan.andraz28@gmail.com>
2021-02-22 13:00:48 +0100

    + News

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210210165810) do
+ActiveRecord::Schema.define(version: 20210222115829) do
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.string "caption"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
